25f7d089d7576be29bd900f13d98f78303a2b719
szydlovski <kamil.szydlovski@gmail.com>
2021-01-07 03:20:24 +0100

    Removed temp file

--- dev.js
+++
@@ -1,9 +0,0 @@
-import {readSwatchesFile, createSwatchesFile} from './src/index.js';
-import fs from 'fs';
-
-(async () => {
-  const data = fs.readFileSync('./test/sample/mypalette.swatches');
-  console.log(data);
-  const swatches = await readSwatchesFile(data);
-  console.log(swatches);
-})();
